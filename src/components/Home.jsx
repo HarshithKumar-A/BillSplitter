f41157a6e4025fe38bf6b5311cf861a66c76dcd5
harshith <harshith@qburst.com>
2023-11-19 18:07:25 +0530

    use grids in Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 import { fetchData } from '../API/api';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBill, faHistory, faChartPie, faComments, faList, faUser } from '@fortawesome/free-solid-svg-icons';
+import Updates from './Updates';
 
 function Home() {
   const navigate = useNavigate();
+
   useEffect(() => {
     const firebaseConfig = {
       apiKey: "AIzaSyB6c1DCp7f_f-ufTLflyWvqayFfYc4Id-I",
@@ -52,7 +56,17 @@ function Home() {
   }, [])
 
 
+  // Array containing options for each card in the grid
+  const options = [
+    { title: 'Create New Split', link: '/new-split', icon: faMoneyBill },
+    { title: 'View History', link: '/view-history', icon: faHistory },
+    { title: 'View Summary', link: '/summary', icon: faChartPie },
+    { title: 'Discuss', link: '/chat', icon: faComments },
+    { title: 'Unpublished Splits', link: '/unpublished', icon: faList },
+    { title: 'Switch User', action: () => { localStorage.removeItem('v1:userInfo'); navigate('/login') }, icon: faUser },
+  ];
 
+  // Function to show notifications
   function showNotification(title, options) {
     if (Notification.permission === "granted") {
       const notification = new Notification(title, options);
@@ -70,66 +84,20 @@ function Home() {
 
 
   return (
-    <div className="container mt-3">
+    <div className="container home-container mt-3">
       <div className="row">
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Create New Split</h5>
-              <p className="card-text">Split expenses equally among friends.</p>
-              <Link to="/new-split" className="btn btn-primary">Go to New Split</Link>
-            </div>
+        {options.map((option, index) => (
+          <div onClick={option.action} key={index} className="col-sm-12 col-6 col-md-4 col-lg-2 mb-3">
+            <Link to={option.link} className="text-decoration-none">
+              <div className="card card-1 p-3 text-center shadow">
+                <FontAwesomeIcon icon={option.icon} size="4x" className="mb-3" />
+              </div>
+              <p className="text-center mt-2">{option.title}</p>
+            </Link>
           </div>
-        </div>
-
-        <div className="col-md-4 pt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">View History</h5>
-              <p className="card-text">View your expense history.</p>
-              <Link to="/view-history" className="btn btn-primary">Go to View History</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 pt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">View Summary</h5>
-              <p className="card-text">View a summary of your expenses.</p>
-              <Link to="/summary" className="btn btn-primary">Go to Summary</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 pt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">discuss</h5>
-              <p className="card-text">View a discuss of your team.</p>
-              <Link to="/chat" className="btn btn-primary">Go to discuss</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 pt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Unpublished Splits</h5>
-              <p className="card-text">Publish Unpublished Split</p>
-              <Link to="/unpublished" className="btn btn-primary">Go to Unpublished Splits</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-4 pt-2">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Switch User</h5>
-              <p className="card-text"></p>
-              <button onClick={() => { localStorage.removeItem('v1:userInfo'); navigate('/login') }} className="btn btn-primary">Go to Login</button>
-            </div>
-          </div>
+        ))}
+        <div className="col-12">
+          <Updates></Updates>
         </div>
       </div>
     </div>
---
+++ src/components/Updates.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+
+const Updates = () => {
+    const [nextUpdate, setNextUpdate] = useState(null);
+
+    const schedule = [
+        { place: 'CLT', arrival: null, departure: '2023-12-06T17:10:00' },
+        { place: 'Mumbai', arrival: '2023-12-07T17:05:00', departure: '2023-12-07T23:25:00' },
+        { place: 'Udaipur', arrival: '2023-12-08T14:40:00', departure: '2023-12-10T06:00:00' },
+        { place: 'Ajmer', arrival: '2023-12-10T23:10:00', departure: '2023-12-11T12:20:00' },
+        { place: 'Jaisalmer', arrival: '2023-12-10T22:30:00', departure: '2023-12-13T23:15:00' },
+        { place: 'Jaipur', arrival: '2023-12-14T10:15:00', departure: '2023-12-15T17:35:00' },
+        { place: 'Delhi', arrival: '2023-12-15T22:30:00', departure: '2023-12-16T05:40:00' },
+        { place: 'CLT', arrival: '2023-12-18T03:32:00', departure: null },
+    ];
+
+    useEffect(() => {
+        const findNextDeparture = () => {
+            const now = new Date();
+            const upcomingDeparture = schedule.find(
+                (event) => event.departure && new Date(event.departure) > now
+            );
+
+            if (upcomingDeparture) {
+                const timeDifference = new Date(upcomingDeparture.departure) - now;
+                setNextUpdate({
+                    place: upcomingDeparture.place,
+                    arrival: upcomingDeparture.arrival,
+                    timer: timeDifference,
+                });
+            } else {
+                setNextUpdate(null);
+            }
+        };
+
+        findNextDeparture();
+        const timerId = setInterval(findNextDeparture, 1000);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(timerId);
+    }, []);
+
+    const formatTime = (time) => {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+        return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+    };
+
+    return (
+        <div className="card p-3 text-center shadow">
+            {nextUpdate ? (
+                <p className="fade-in">
+                    Next train from {nextUpdate.place} within {formatTime(nextUpdate.timer)}
+                </p>
+            ) : (
+                <p>No upcoming updates</p>
+            )}
+        </div>
+    );
+};
+
+export default Updates;
