b47199d166284c2eab49f5124cfedf4918f63134
Harshith <harshith@qburst.com>
2024-12-29 16:59:51 +0530

    fina;

--- src/API/api.js
+++ src/API/api.js
@@ -4,7 +4,7 @@ import { getTrip } from "./localStorage";
 const BASE_URL = [
     'https://script.google.com/macros/s/AKfycbxcUwY54G5RraEfAcM5OedToG8xREMIkf84-KGkAjo-UB_S57l0KSX8djVT2GIyooFYRQ/exec',
     'https://script.google.com/macros/s/AKfycbxRs84pGXVe5h-TKqf4XnNA_WJt9lx5z6cPQuOV6wEkRHv13E6Y5PILf2CQ6gMrCqU7nQ/exec',
-    'https://script.google.com/macros/s/AKfycbwLOEASu83cZdri7fcYDR8xP4yGqlJJp2qzJUxXHGTgcFu5TK5_VApKqCJkeIxDpCgMxA/exec',
+    'https://script.google.com/macros/s/AKfycbwjWPCauqBqHCE8sOastaXehpkWgrhdhPr259-99vr2baAXpBG7KXE_MfZ4JafGxTplcw/exec',
 ]
 
 export const fetchData = async (action, payload) => {
